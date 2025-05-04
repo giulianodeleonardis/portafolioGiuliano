37e6d4e22d3ef191f38d77fa3d8862b235388b14
Giuliano <deleonardis86@hotmail.com>
2025-05-04 22:03:39 +0100

    Fixing checked list

--- assets/js/wordSearch.js
+++ assets/js/wordSearch.js
@@ -203,7 +203,7 @@ function displayWordListToSelect(words, container) {
                 inputTag.setAttribute("name", nameForEachCheck);
                 inputTag.setAttribute("id", nameForEachCheck);
                 inputTag.setAttribute("class", "inputTypeCheck");
-                //inputTag.setAttribute("checked", true);
+                inputTag.setAttribute("checked", false);
                 labelTag.appendChild(inputTag);
                 labelTag.appendChild(labelText);
                 divItem.appendChild(labelTag);
